48e2c9271fbb0e7eba423f9be5da1d9e04fbe938
Jerrod Blavos <jerrod@users.noreply.github.com>
2023-04-15 02:39:13 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-var extract = require('ipa-extract-info');
-var input = document.querySelector('input');
+import JSZip from 'jszip';
+import plist from 'plist';
 
-console.log('input', input);
-input.addEventListener('change', function(){
-  extract(input.files[0], function(err, info, raw){
-    if (err) throw err;
-    console.log('info', info); // the parsed plist
-    console.log('raw', raw);   // the unparsed plist
-  });
+document.getElementById('ipaFileInput').addEventListener('change', (event) => {
+  const ipaFile = event.target.files[0];
+  if (!ipaFile) return;
+
+  const zip = new JSZip();
+  const reader = new FileReader();
+
+  reader.onload = async (e) => {
+    try {
+      const ipaData = e.target.result;
+      const loadedZip = await zip.loadAsync(ipaData);
+
+      const appFolderName = Object.keys(loadedZip.files).find((file) => file.includes('.app/'));
+      const plistPath = `${appFolderName}Info.plist`;
+
+      const plistFile = await loadedZip.file(plistPath).async('uint8array');
+      const plistContent = new TextDecoder().decode(plistFile);
+      const plistObject = plist.parse(plistContent);
+
+      console.log('Plist:', plistObject);
+    } catch (error) {
+      console.error('Error processing IPA file:', error);
+    }
+  };
+
+  reader.onerror = (error) => {
+    console.error('Error reading IPA file:', error);
+  };
+
+  reader.readAsArrayBuffer(ipaFile);
 });
